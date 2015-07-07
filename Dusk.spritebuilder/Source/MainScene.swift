0b098c8aea93ab9c522076ecb633343801644b7b
Alan Huang <alanyuhuang@gmail.com>
2015-07-07 13:19:14 -0700

    Added background music

--- Dusk.spritebuilder/Source/MainScene.swift
+++ Dusk.spritebuilder/Source/MainScene.swift
@@ -1,8 +1,11 @@
 import Foundation
-
 class MainScene: CCNode {
     func start(){
+        OALSimpleAudio.sharedInstance().playBg("GameMusic.mp3")
+        OALSimpleAudio.sharedInstance().playBgWithLoop(true)
         let gameplayScene = CCBReader.loadAsScene("Gameplay")
         CCDirector.sharedDirector().presentScene(gameplayScene)
+        
+        
     }
 }
