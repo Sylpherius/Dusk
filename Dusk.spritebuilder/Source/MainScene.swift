1ff1182ccc39d00cd9019f3fc00707579582f5ad
Alan Huang <alanyuhuang@gmail.com>
2015-07-21 12:59:02 -0700

    Just commented out potential leafageness

--- Dusk.spritebuilder/Source/MainScene.swift
+++ Dusk.spritebuilder/Source/MainScene.swift
@@ -1,13 +1,12 @@
 import Foundation
 class MainScene: CCNode {
-    weak var titlePhysicsNode: CCPhysicsNode!
-    var leaf: Leaf?
+//    weak var titlePhysicsNode: CCPhysicsNode!
+//    var leaf: Leaf?
+//    var leafTime: CCTime = 0
     
     func didLoadFromCCB(){
         OALSimpleAudio.sharedInstance().playBg("GameMusic.mp3")
         OALSimpleAudio.sharedInstance().playBgWithLoop(true)
-        OALSimpleAudio.sharedInstance().preloadEffect("Sound/Wind.wav")
-        //OALSimpleAudio.sharedInstance().playEffect("Sound/Wind.wav")
     }
     func start(){
         if NSUserDefaults.standardUserDefaults().integerForKey("highscore") == 0{
@@ -19,7 +18,17 @@ class MainScene: CCNode {
         }
     }
     override func update(delta: CCTime) {
-        leaf = CCBReader.load("Leaf") as! Leaf?
-        var xLeaf = Int(CCRANDOM_0_1() * 300 - 150)
+//        leafTime += delta
+//        leaf = CCBReader.load("Leaf") as! Leaf?
+//        var xLeaf = Int(CCRANDOM_0_1() * 300 - 150)
+//        if leafTime >= 0.1 {
+//            self.leafTime = 0
+//            if let leaf = leaf {
+//                titlePhysicsNode.addChild(leaf)
+//                let leafPos = titlePhysicsNode.convertToWorldSpace(CGPoint(x: xLeaf, y: 0))
+//                leaf.position = titlePhysicsNode.convertToNodeSpace(leafPos)
+//            }
+//        }
+        
     }
 }
